54fe6510ebad389c1c93918be29ea114d8c5922e
Eric <61258279+harowo@users.noreply.github.com>
2020-07-01 23:10:14 +0800

    Update index.js

--- index.js
+++ index.js
@@ -124,7 +124,7 @@ let acronymAbyss = new cron.CronJob('00 12 * * 0,3', test => {
 });
 
 // experimental
-let scheduledXdress = new cron.CronJob('10 15 * *', test => {
+let scheduledXdress = new cron.CronJob('11 15 * *', test => {
   // This runs every Tue and Sun at 20:00:00
   // 2000 local is 1200 
   let bot_test = client.channels.cache.get(`596785780953841694`)
