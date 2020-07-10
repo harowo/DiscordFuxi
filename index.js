0c4345349886d0ab54705826d82b2f091dff04de
Eric <61258279+harowo@users.noreply.github.com>
2020-07-10 21:37:14 +0800

    Updated MYR table credits

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ client.on("message", message => {
   } else if (message.content.toLowerCase() == "&swipe sgd") {
     message.channel.send(`${message.author} Pls spend wisely (ty erine)\nhttps://cdn.discordapp.com/attachments/653641484834439169/714907595789303869/unknown.png`);
   } else if (message.content.toLowerCase() == "&swipe myr") {
-    message.channel.send(`${message.author} Pls spend wisely (ty Holiday's cadet)\nhttps://cdn.discordapp.com/attachments/622385222541115403/654583837749280768/unknown.png`);
+    message.channel.send(`${message.author} Pls spend wisely (ty Kamazima)\nhttps://cdn.discordapp.com/attachments/622385222541115403/654583837749280768/unknown.png`);
   } else if (message.content.toLowerCase() == "&swipe php") {
     message.channel.send(`${message.author} Pls spend wisely (ty NeonGenesis)\nhttps://cdn.discordapp.com/attachments/575004401551278105/727451161849888808/unknown.png`);
   } else if (message.content.toLowerCase() == "hecchi") {
