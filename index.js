bc261ca401afe9d0fa99be804565d1af4caee9b1
Eric <61258279+harowo@users.noreply.github.com>
2020-07-24 19:00:58 +0800

    Removed acronym abyss

--- index.js
+++ index.js
@@ -105,14 +105,6 @@ let abyssTwothreethree = new cron.CronJob('0 12 * * 0,3', test => {
   huangwu_zh.send(attachment)
 });
 
-// Abyss reminder in acronym
-let acronymAbyss = new cron.CronJob('00 12 * * 0,3', test => {
-  // This runs every Tue and Sun at 20:00:00
-  // 2000 local is 1200 
-  let general_channel = client.channels.cache.get(`486874570532585483`)
-  general_channel.send("you need the crystals, climb abyss! " + '<@&721737831357349959>')
-  // pings Abyss Ping role
-});
 
 /* experimental
 let scheduledXdress = new cron.CronJob('* * * * *', test => {
@@ -127,7 +119,7 @@ scheduledXdress.start()
 
 scheduledMessage.start()
 abyssTwothreethree.start()
-acronymAbyss.start()
+
 
 client.on('message', (receivedMessage) => {
   if (receivedMessage.author == client.user) { // Prevent bot from responding to its own messages
