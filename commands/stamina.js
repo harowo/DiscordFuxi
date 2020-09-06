932417f8e5b60981106ac39151aeb838de68e62a
Eric <61258279+harowo@users.noreply.github.com>
2020-09-07 00:53:43 +0800

    Update stamina.js

--- commands/stamina.js
+++ commands/stamina.js
@@ -11,20 +11,19 @@ module.exports = {
         const regenRate = 0.1 // 1 stam regens every 6min
         var currStam = args
         var remainTime = Math.round(((maxStam - currStam) * regenRate) * 10) / 10
-        process.env.TZ = 'Asia/Singapore'
 
-        var currDate = new Date(Date.now())
-        var nowInms = currDate.getTime()
-        var remainTimems = remainTime * 60 * 60 * 1000 // convert remainTime to milliseconds
-        var futureDateTimems = nowInms + remainTimems;
-        var futureTime = new Date(futureDateTimems)
+        //var currDate = new Date(Date.now())
+        //var nowInms = currDate.getTime()
+        //var remainTimems = remainTime * 60 * 60 * 1000 // convert remainTime to milliseconds
+        //var futureDateTimems = nowInms + remainTimems;
+        //var futureTime = new Date(futureDateTimems)
 
         if(args == undefined || !args || args >= maxStam || args < 0) {
             message.channel.send("<:slap:693351841404026890>");
             return;
         }
         const stamMsg = new Discord.MessageEmbed()
-          .setDescription("It is approximately " + `${remainTime}` + " hours from full stamina.\n\nIt will be " + futureTime)
+          .setDescription("It is approximately " + `${remainTime}` + " hours from full stamina.)
           .setColor('#0099ff');
 
         message.channel.send(stamMsg)
