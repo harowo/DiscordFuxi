43d27b2998a5cd896d36a679361f2650a719c0e7
Eric <wang.yaohui@dhs.sg>
2020-09-16 11:20:11 +0800

    added swipe php

--- commands/swipe.js
+++ commands/swipe.js
@@ -9,10 +9,11 @@ module.exports = {
         
         var arrIndex = 0;
         var currency = args
-        var swipeCurrency = ["sgd", "myr"]
+        var swipeCurrency = ["sgd", "myr", "php"]
         var swipeMsg = [
         `${message.author} Pls spend wisely (ty erine)\nhttps://cdn.discordapp.com/attachments/653641484834439169/714907595789303869/unknown.png`,
-        `${message.author} Pls spend wisely (ty Holiday's cadet)\nhttps://cdn.discordapp.com/attachments/622385222541115403/654583837749280768/unknown.png`
+        `${message.author} Pls spend wisely (ty Holiday's cadet)\nhttps://cdn.discordapp.com/attachments/622385222541115403/654583837749280768/unknown.png`,
+        `${message.author} Pls spend wisely (ty M)\nhttps://cdn.discordapp.com/attachments/575004401551278105/754630841770180668/unknown.png`
     ]
         
         if (swipeCurrency.toString().includes(args)) {
